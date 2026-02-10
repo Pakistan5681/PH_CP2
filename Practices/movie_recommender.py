f45a684f67ee5638dbadd3ac4c0a6e404fc6f16c
Pakistan5681 <paxton.hall@ucas-edu.net>
2026-02-10 19:17:46 +0000

    more csv

--- Practices/movie_recommender.py
+++ Practices/movie_recommender.py
@@ -21,28 +21,55 @@ def menu():
 
 
 def search_or_recommend():
-    with open("Practices\\Movies list - Sheet1 (1).csv", "r") as file:
-        reader = csv.reader(file)
+    with open("Practices/Movies list - Sheet1 (1).csv", "r") as file:
+        reader = list(csv.reader(file))
 
         print("1. Genre")
         print("2. Director")
         print("3. Actor")
         print("4. Length")
 
-        search_method = input("Choose your filters")
+        search_method = input("Choose your filters ")
         filters_raw = search_method.split(",")
         filters = {}
 
-        if "1" in filters: 
-            filters.append(1)
+        if "1" in filters_raw: 
+            genreToAdd = input("What genre do you want to sort by? ")
+            filters[1] = genreToAdd.strip().capitalize()
 
+        if "2" in filters_raw:
+            director = input("What director do you want to sort by? ")
+            filters[2] = director.strip().capitalize()
 
-        if "2" in filters: filters.append(2)
-        if "3" in filters: filters.append(3)
-        if "4" in filters: filters.append(4)
+        if "3" in filters_raw:
+            actor = input("What actor do you want to sort by? ")
+            filters[3] = actor.strip().capitalize()
+        
+        if "4" in filters_raw:
+            legnthMin = pf.idiot_proof_general("What is the shortest time (in minutes) the movie can be ")
+            legnthMax = pf.idiot_proof_general("What is the longest time (in minutes) the movie can be ")
+            filters[4] = (legnthMin, legnthMax)
 
+        potentail_films = []
         for row in reader:
-            print(row[1])
+            potentail_films.append(", ".join(row))
+
+        
+
+        if 1 in filters.keys():
+            for row in reader:
+                if  "/" in row[2]: genres = row[2].split("/")
+                else: genres= row[2]
+
+                print(row[2])
+
+                if not row[2] in genres:
+                    genres = row
+                    potentail_films.remove(", ".join(row))
+
+        for i in potentail_films:
+            print(i)
+            
 
 
 
