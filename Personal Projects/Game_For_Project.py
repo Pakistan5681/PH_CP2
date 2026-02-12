18cc7f5e102610dbea083434e43d5d5a26f0298c
Pakistan5681 <paxton.hall@ucas-edu.net>
2026-02-12 10:51:24 -0700

    Flesh Cube II done

--- Personal Projects/Game_For_Project.py
+++ Personal Projects/Game_For_Project.py
@@ -1,158 +1,159 @@
 import pygame as py
 from random import randint
-from time import sleep
 
-py.init()
+def runFleshCubeII():
+    py.init()
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
+    SCREEN_WIDTH = 1920
+    SCREEN_HEIGHT = 1080
 
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-BROWN = (155, 118, 83)
-PLAYER_COLOR = (242, 177, 97)
-SKY_BLUE = (135, 206, 235)
-YELLOW = (255, 255, 0)
+    RED = (255, 0, 0)
+    BLACK = (0, 0, 0)
+    GREEN = (0, 255, 0)
+    BROWN = (155, 118, 83)
+    PLAYER_COLOR = (242, 177, 97)
+    SKY_BLUE = (135, 206, 235)
+    YELLOW = (255, 255, 0)
 
-running = True
+    running = True
 
-screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = py.time.Clock()
-py.display.set_caption("Flesh Cube II")
+    screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = py.time.Clock()
+    py.display.set_caption("Flesh Cube II")
 
-hazards = [] # position (on the x axis) of every hazard
+    hazards = [] # position (on the x axis) of every hazard
 
-ground = (SCREEN_HEIGHT / 12) * 9 # The lowest point the player can be
+    ground = (SCREEN_HEIGHT / 12) * 9 # The lowest point the player can be
 
-fps = 60
-max_fps = 500
+    fps = 60
+    max_fps = 500
 
-score = 0
-highScore = 0
+    score = 0
+    highScore = 0
 
-font = py.font.Font(None, 36)
+    font = py.font.Font(None, 36)
 
-spike_colliders = []
+    spike_colliders = []
 
-PLAYER_X = SCREEN_WIDTH / 8 
-playerY = ground 
+    PLAYER_X = SCREEN_WIDTH / 8 
+    playerY = ground 
 
-PLAYER_WIDTH = SCREEN_WIDTH / 17
-PLAYER_HEIGHT = SCREEN_HEIGHT / 5
+    PLAYER_WIDTH = SCREEN_WIDTH / 17
+    PLAYER_HEIGHT = SCREEN_HEIGHT / 5
 
-SPIKE_WIDTH = 75
-SPIKE_HEIGHT = 75
+    SPIKE_WIDTH = 75
+    SPIKE_HEIGHT = 75
 
-moveSpeed = 5
+    moveSpeed = 5
 
-yVel = 0
-gravity = 0.5
+    yVel = 0
+    gravity = 0.5
 
-touchingGround = True
+    touchingGround = True
 
-spawnTimer = 0
-spawnEveryXFrames = 90
+    spawnEveryXFrames = 90
+    spawnTimer = 0
 
-def endGame():
-    return score, highScore
+    with open("Personal Projects\scoreSaver.txt", "r") as file:
+        highScore = int(file.read())
 
-with open("Personal Projects\scoreSaver.txt", "r") as file:
-    highScore = int(file.read())
+    while running:
+        screen.fill(SKY_BLUE)
 
-while running:
-    screen.fill(SKY_BLUE)
+        clock.tick(fps)
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                running = False 
 
-    clock.tick(fps)
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            running = False 
+        keys = py.key.get_pressed()
 
-    keys = py.key.get_pressed()
+        if keys[py.K_SPACE]: 
+            if touchingGround:
+                yVel = -20
+                touchingGround = False
 
-    if keys[py.K_SPACE]: 
-        if touchingGround:
-            yVel = -20
-            touchingGround = False
+        if not touchingGround: yVel += gravity
+        playerY += yVel
 
-    if not touchingGround: yVel += gravity
-    playerY += yVel
-    
-    if playerY >= ground and not touchingGround:
-        touchingGround = True
-        playerY = ground
-        yVel = 0
+        if playerY >= ground and not touchingGround:
+            touchingGround = True
+            playerY = ground
+            yVel = 0
 
-    print(f"{fps}")
+        player_rect = py.Rect(
+            PLAYER_X,
+            playerY - PLAYER_HEIGHT,
+            PLAYER_WIDTH,
+            PLAYER_HEIGHT
+        )
 
-    player_rect = py.Rect(
-        PLAYER_X,
-        playerY - PLAYER_HEIGHT,
-        PLAYER_WIDTH,
-        PLAYER_HEIGHT
-    )
+        py.draw.rect(screen, BROWN, (0, ground, SCREEN_WIDTH, ground)); py.draw.rect(screen, GREEN, (0, ground, SCREEN_WIDTH, ground / 15))
+        py.draw.rect(screen, PLAYER_COLOR, player_rect)
 
-    py.draw.rect(screen, BROWN, (0, ground, SCREEN_WIDTH, ground)); py.draw.rect(screen, GREEN, (0, ground, SCREEN_WIDTH, ground / 15))
-    py.draw.rect(screen, PLAYER_COLOR, player_rect)
+        py.draw.ellipse(screen, YELLOW, (1800, 100, 100, 100))
 
-    py.draw.ellipse(screen, YELLOW, (1800, 100, 100, 100))
+        spawnTimer += 1
 
-    spawnTimer += 1
+        if spawnTimer == spawnEveryXFrames:
+            hazards.append(SCREEN_WIDTH + SPIKE_WIDTH)
+            spawnTimer = 0
+            spawnEveryXFrames = randint(90, 240)
 
-    if spawnTimer == spawnEveryXFrames:
-        hazards.append(SCREEN_WIDTH + SPIKE_WIDTH)
-        spawnTimer = 0
-        spawnEveryXFrames = randint(90, 240)
-
-        spike_colliders.append(py.Rect(
-            SCREEN_WIDTH,
-            ground - SPIKE_HEIGHT,
-            SPIKE_WIDTH,
-            SPIKE_HEIGHT
-        ))
-
-    text_surface = font.render(str(score), True, BLACK)
-    text_surface_two = font.render(str(highScore), True, YELLOW)
-
-    screen.blit(text_surface, (100, 100, 100, 100))
-    screen.blit(text_surface_two, (100, 200, 100, 100))
-
-    if score > highScore:
-        highScore = score
-
-
-    for i in spike_colliders:
-        if player_rect.colliderect(i):
-            running = False
-
-    for i in range(len(hazards)):
-        if len(hazards) - 1 >= i:
-            hazards[i] -= moveSpeed
-
-            spike_rect = py.Rect(
-                hazards[i] - SPIKE_WIDTH,
+            spike_colliders.append(py.Rect(
+                SCREEN_WIDTH,
                 ground - SPIKE_HEIGHT,
                 SPIKE_WIDTH,
                 SPIKE_HEIGHT
-            )
+            ))
 
-            py.draw.rect(screen, RED, spike_rect)
+        text_surface = font.render(str(score), True, BLACK)
+        text_surface_two = font.render(str(highScore), True, YELLOW)
 
-            if player_rect.colliderect(spike_rect):
-                running = False        
+        screen.blit(text_surface, (100, 100, 100, 100))
+        screen.blit(text_surface_two, (100, 200, 100, 100))
 
-            if hazards[i] == PLAYER_X:
-                score += 1
-            if hazards[i] < -100:
-                hazards.remove(hazards[i])
+        if score > highScore:
+            highScore = score
 
-    if fps < max_fps: fps += 0.05
 
-    if running: py.display.flip()
+        for i in spike_colliders:
+            if player_rect.colliderect(i):
+                running = False
 
-endGame()
+        for i in range(len(hazards)):
+            if len(hazards) - 1 >= i:
+                hazards[i] -= moveSpeed
 
-with open("Personal Projects\scoreSaver.txt", "w") as file:
-    file.write(str(highScore))
+                spike_rect = py.Rect(
+                    hazards[i] - SPIKE_WIDTH,
+                    ground - SPIKE_HEIGHT,
+                    SPIKE_WIDTH,
+                    SPIKE_HEIGHT
+                )
 
+                py.draw.rect(screen, RED, spike_rect)
+
+                if player_rect.colliderect(spike_rect):
+                    running = False        
+
+                if hazards[i] == PLAYER_X:
+                    score += 1
+                if hazards[i] < -100:
+                    hazards.remove(hazards[i])
+
+        if fps < max_fps: fps += 0.05
+
+        if running: py.display.flip()
+
+    with open("Personal Projects\scoreSaver.txt", "w") as file:
+        file.write(str(highScore))
+
+    YELLOWTEXT = "\033[33m"
+
+    print(" ")
+    print(f"{YELLOWTEXT}Score: {score}")
+    print(" ")
+    return score, highScore
+
+runFleshCubeII()
 
