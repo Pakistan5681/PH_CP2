009419edf75beb983fe8603e8ded903867c1359a
Pakistan5681 <paxton.hall@ucas-edu.net>
2026-03-05 10:51:22 -0700

    Upgrades Work

--- Personal Projects/RogueLike.py
+++ Personal Projects/RogueLike.py
@@ -2,27 +2,28 @@ import pygame as py
 from random import choice, randint
 
 class Upgrade:
-    def __init__(self, name, stat, amount, rarity):
+    def __init__(self, name, stat, amount, rarity, description):
         self.name = name
         self.stat = stat
         self.amount = amount
+        self.rarity = rarity
+        self.description = description
 
 py.init()
 
 potUpgrades = [
-    Upgrade("Vitality", "maxHealth", 1, "common"),
-    Upgrade("Big Vitality", "maxHealth", 2, "rare"),
-    Upgrade("Huge Vitality", "maxHealth", 3, "epic"),
-    Upgrade("Pakistinian Vitality", "maxHealth", 5, "pakistinian"),
-    Upgrade("Haste", "playerSpeed", 1, "common"),
-    Upgrade("Quick", "playerSpeed", 2, "rare"),
-    Upgrade("Lightning Fast", "playerSpeed", 3, "epic"),
-    Upgrade("Speed of Pakistan", "playerSpeed", 5, "pakistinian"),
-    Upgrade("Quick Hands", "reloadTime", 0.9, "common"),
-    Upgrade("Dexterous", "reloadTime", 0.8, "rare"),
-    Upgrade("Reloader", "reloadTime", 0.7, "epic"),
-    Upgrade("Pakistinian Dexterity", 0.55, "pakistinian"),
-    Upgrade("Full Heal", "health", 0, "rare")
+    Upgrade("Vitality", "Max Health", 1, "common", "Increases Max Health by 1"),
+    Upgrade("Big Vitality", "Max Health", 2, "rare", "Increases Max Health by 2"),
+    Upgrade("Huge Vitality", "Max Health", 3, "epic", "Increases Max Health by 3"),
+    Upgrade("Pakistinian Vitality", "Max Health", 5, "pakistinian", "Increases Max Health by 5"),
+    Upgrade("Haste", "Player Speed", 1, "common", "Increases Speed by 1"),
+    Upgrade("Quick", "Player Speed", 2, "rare", "Increases Speed by 2"),
+    Upgrade("Lightning Fast", "Player Speed", 3, "epic", "Increases Speed by 3"),
+    Upgrade("Speed of Pakistan", "Player Speed", 5, "pakistinian", "Increases Speed by 5"),
+    Upgrade("Quick Hands", "Reload Time", 0.9, "common", "Increases Reload Speed"),
+    Upgrade("Dexterous", "Reload Time", 0.8, "rare", "Greatly Increases Reload Speed"),
+    Upgrade("Reloader", "Reload Time", 0.7, "epic", "Massively Increases Reload Speed"),
+    Upgrade("Pakistinian Dexterity", "Reload Time", 0.55, "pakistinian", "Increases Reload Speed to an Unreasonable Degree")
 ]
 
 SCREEN_WIDTH = 1920
@@ -33,10 +34,12 @@ GREEN = (6, 69, 23)
 BROWN = (82, 49, 2)
 PLAYER_COLOR = (242, 177, 97)
 SKY_BLUE = (2, 78, 122)
-YELLOW = (255, 255, 0)
+YELLOW = (194, 177, 27)
 WHITE = (255, 255, 255)
 GREY = (79, 79, 79)
 XP_GREEN = (0, 255, 38)
+BLUE = (7, 0, 112)
+PURPLE = (143, 0, 214)
 
 running = True
 screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -68,8 +71,18 @@ health = 3
 xp = 0
 upgradeReq = 10
 
+font = py.font.Font(None, 36)
+bigFont = py.font.Font(None, 72)
+
 xp_bar_lengnth = 500
 xp_bar_start = 25
+
+def getColor(rarity):
+    match rarity:
+        case "common": return GREY
+        case "rare": return BLUE
+        case "epic": return PURPLE
+        case "pakistinian": return YELLOW
 
 def get_foe_pos(): # For spawning foes, spawns a foe outside of the screen
     global SCREEN_WIDTH
@@ -93,6 +106,56 @@ def get_foe_pos(): # For spawning foes, spawns a foe outside of the screen
         out.append(SCREEN_HEIGHT + 150)
 
     return out
+
+def LevelUpOptions(playerSpeed, playerHealth, maxHealth, bulletReload):
+    global potUpgrades
+
+    def applyUpgrade(playerSpeed, playerHealth, maxHealth, bulletReload, stat, amount):
+        match stat:
+            case "Max Health":
+                maxHealth += amount
+                playerHealth = maxHealth
+            case "Player Speed":
+                playerSpeed += amount
+            case "Reload Time":
+                bulletReload *= amount
+
+        return playerSpeed, playerHealth, maxHealth, bulletReload
+
+
+    upgrades = []
+    rects = []
+    for i in range(3): 
+        upgrades.append(choice(potUpgrades))
+        rect = py.Rect(i * (SCREEN_WIDTH / 3) + 12, 100, (SCREEN_WIDTH / 3) - 25, SCREEN_HEIGHT - 200)
+        rects.append(rect)
+
+    while True:
+        screen.fill(WHITE)
+        texty = bigFont.render("Choose An Upgrade!", True, BLACK)
+        screen.blit(texty, (25, 25, SCREEN_WIDTH - 50, 50))
+          
+        for i in range(3):   
+            color = getColor(upgrades[i].rarity)
+            py.draw.rect(screen, color, rects[i])
+
+            text_surface = font.render(upgrades[i].name, True, WHITE)
+            screen.blit(text_surface, (i * (SCREEN_WIDTH / 3) + 25, 200, (SCREEN_WIDTH / 3) - 75, 100))
+
+            text_surface_two = font.render(upgrades[i].description, True, WHITE)
+            screen.blit(text_surface_two, (i * (SCREEN_WIDTH / 3) + 25, 300, (SCREEN_WIDTH / 3) - 75, 100))
+
+        for event in py.event.get():
+            if event.type == py.MOUSEBUTTONDOWN:
+                if rects[0].collidepoint(event.pos):
+                    return applyUpgrade(playerSpeed, playerHealth, maxHealth, bulletReload, upgrades[0].stat, upgrades[0].amount)
+                elif rects[1].collidepoint(event.pos):
+                    return applyUpgrade(playerSpeed, playerHealth, maxHealth, bulletReload, upgrades[1].stat, upgrades[1].amount)
+                elif rects[2].collidepoint(event.pos):
+                    return applyUpgrade(playerSpeed, playerHealth, maxHealth, bulletReload, upgrades[2].stat, upgrades[2].amount)
+
+        py.display.flip()
+            
 
 while running:
     clock.tick(60)
@@ -215,10 +278,11 @@ while running:
         else:
             py.draw.rect(screen, XP_GREEN, (xp_bar_start + (i * (xp_bar_lengnth / upgradeReq)), 75, (xp_bar_lengnth / upgradeReq), 25))
 
+    # Upgrade Logic
     if xp >= upgradeReq:
         xp = 0
         upgradeReq = round(upgradeReq * 1.25)
-        # Logic for level up goes here
+        playerSpeed, health, maxHealth, reloadTime = LevelUpOptions(playerSpeed, health, maxHealth, reloadTime)
 
     # kills the player
     if health <= 0:
@@ -226,8 +290,4 @@ while running:
 
     py.draw.ellipse(screen, WHITE, (playerX, playerY, 100, 100))
     if running: py.display.flip()
-
-def LevelUpOptions():
-    while True:
-        for i in range(3):
-            py.draw.rect(screen, GREY, (i * ((SCREEN_WIDTH / 3) + 50), SCREEN_HEIGHT - 200, ))
+            
